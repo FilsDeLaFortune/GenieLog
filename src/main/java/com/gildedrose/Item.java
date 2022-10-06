94ef7825bb90e472707c4b3818bf8cf4d47c0c4b
FilsDeLaFortune <allardbenjaminpro@gmail.com>
2022-10-06 14:44:27 +0200

    Beautified code (swagg purpose only)

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -5,12 +5,11 @@ public class Item {
     public String name;
 
     public int sellIn;
-
     public int quality;
 
     public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
+        this.name    = name;
+        this.sellIn  = sellIn;
         this.quality = quality;
     }
 
@@ -25,21 +24,13 @@ public class Item {
                 break;
             case "Aged Brie":
                 this.quality++;
-                if(this.sellIn<0){
-                    this.quality++;
-                }
+                if(this.sellIn<0)  this.quality++;
                 break;
             case "Backstage passes to a TAFKAL80ETC concert":
                 this.quality++;
-                if(this.sellIn<10){
-                    this.quality++;
-                }
-                if(this.sellIn<5){
-                    this.quality++;
-                }
-                if(this.sellIn<0){
-                    this.quality=0;
-                }
+                if(this.sellIn<10) this.quality++;
+                if(this.sellIn<5)  this.quality++;
+                if(this.sellIn<0)  this.quality=0;
                 break;
 
             default:
@@ -49,13 +40,14 @@ public class Item {
                 }
                 break;
         }
-        if(this.quality>=50){
+        if(this.quality >= 50){
             this.quality = 50;
         }
-        if(this.quality<0){
-            this.quality=0;
+        if(this.quality < 0){
+            this.quality = 0;
         }
     }
+    
    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
